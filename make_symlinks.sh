4c903e99c81c1397652b37c85eac00d347c6f108
Nikola Savic <NikolaSavic@gmail.com>
2014-03-12 16:01:51 -0400

    fixed typo and reworked symlink script

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -26,9 +26,11 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo 'Moving $file'
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "Symlinkling $file"
     ln -s $dir/$file ~/.$file
 done
+echo '...done'
