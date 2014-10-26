6b3ff00c3cd1c2ffa1a0cb4da80ef1970e4e274d
Nikola Savic <NikolaSavic@gmail.com>
2014-10-26 15:01:07 -0400

    Add curl flag refresher

--- vim-pluggin-importer.sh
+++ vim-pluggin-importer.sh
@@ -8,4 +8,9 @@ mkdir -p ~/.vim/bundle
 # Install Pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+  # curl flag refresher:
+  # -L, --location, follows redirects
+  # -S, --show-error,  When used with -s it makes curl show an error message if it fails.
+  # -s, --silent Silent, or quiet mode. Don't show progress meter or error messages.  Makes Curl mute.
+  # -o, --output <file>, Write  output to <file> instead of stdout.
 echo 'done!'
