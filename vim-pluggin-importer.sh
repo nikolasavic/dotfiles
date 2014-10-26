acd039424f7226fb795808a71347e35f3dbaaf81
Nikola Savic <NikolaSavic@gmail.com>
2014-10-26 16:09:12 -0400

    Add vim-surround, nerdtree & rainbow parens pluggins

--- vim-pluggin-importer.sh
+++ vim-pluggin-importer.sh
@@ -13,4 +13,12 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/
   # -S, --show-error,  When used with -s it makes curl show an error message if it fails.
   # -s, --silent Silent, or quiet mode. Don't show progress meter or error messages.  Makes Curl mute.
   # -o, --output <file>, Write  output to <file> instead of stdout.
-echo 'done!'
+
+# Install Vim-Surround
+git clone git://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
+
+# Install Nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# Install Rainbow Parens
+git clone https://github.com/kien/rainbow_parentheses.vim.git ~/.vim/bundle/rainbow-parens
