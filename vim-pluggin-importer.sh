613887c09422b3b9e1abeaa050533def8ff52499
Nikola Savic <NikolaSavic@gmail.com>
2014-10-26 16:26:56 -0400

    Add a ton of pluggins

--- vim-pluggin-importer.sh
+++ vim-pluggin-importer.sh
@@ -22,3 +22,40 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 # Install Rainbow Parens
 git clone https://github.com/kien/rainbow_parentheses.vim.git ~/.vim/bundle/rainbow-parens
+
+# Install Paredit
+git clone https://github.com/vim-scripts/paredit.vim.git ~/.vim/bundle/paredit
+
+# Install Tlib
+git clone https://github.com/tomtom/tlib_vim.git ~/.vim/bundle/tlib
+
+# Install TSlime
+git clone https://github.com/vim-scripts/tslime.vim.git ~/.vim/bundle/tslime
+
+# Install Vim-Rails
+git clone https://github.com/tpope/vim-rails.git ~/.vim/bundle/vim-rails
+
+# Install Vim-Fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+
+# Install Vim-Endwise
+git clone https://github.com/tpope/vim-endwise.git ~/.vim/bundle/vim-endwise
+
+# Install Vim-Fireplace
+git clone https://github.com/tpope/vim-fireplace.git ~/.vim/bundle/vim-fireplace
+
+# Install Vim-Dispatch
+git clone https://github.com/tpope/vim-dispatch.git ~/.vim/bundle/vim-dispatch
+
+# Install Vim-Projectionist
+git clone https://github.com/tpope/vim-projectionist.git ~/.vim/bundle/vim-projectionist
+
+# Install Vim-Bundle
+git clone https://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundle
+
+# Install Vim-Heroku
+git clone https://github.com/tpope/vim-heroku.git ~/.vim/bundle/vim-heroku
+
+# Install Vim-Commentary
+git clone https://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
+
