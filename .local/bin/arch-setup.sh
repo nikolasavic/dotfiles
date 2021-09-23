835c6595a5a658833af56d1417e1ffebc77eb958
Nikola Savic <nikolasavic@fastmail.com>
2021-09-23 11:50:32 -0400

    Add notes and reference

--- .local/bin/arch-setup.sh
+++ .local/bin/arch-setup.sh
@@ -11,3 +11,11 @@ echo "*\$* Installing prompt"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 echo "*\$* Arch Setup complete!"
+
+# Rebuild arch-pkglist.txt
+#   `pacman -Qqe > pkglist.txt`
+#
+# note: AUR packages will need to be removed from this list manually
+# Reference:
+#   https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#List_of_installed_packages
+#   https://ostechnix.com/create-list-installed-packages-install-later-list-arch-linux/
